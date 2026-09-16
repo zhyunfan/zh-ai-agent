0acd489617e876bad06f8a6497e5af40e75a50d0
z yf <1344195062@qq.com>
2026-09-16 21:33:04 +0800

    多查询扩展

---
+++ src/main/java/com/zyf/zhaiagent/demo/rag/MultiQueryExpanderDemo.java
@@ -0,0 +1,30 @@
+package com.zyf.zhaiagent.demo.rag;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.rag.Query;
+import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MultiQueryExpanderDemo {//多查询扩展
+
+    private final ChatClient.Builder chatClientBuilder;
+
+    public MultiQueryExpanderDemo(ChatModel dashscopeChatModel){
+        this.chatClientBuilder=ChatClient.builder(dashscopeChatModel);
+    }
+
+    public List<Query> expand(String query){
+        MultiQueryExpander queryExpander = MultiQueryExpander.builder()
+                .chatClientBuilder(chatClientBuilder)
+                .numberOfQueries(3)
+                .build();
+        List<Query> queries = queryExpander.expand(new Query("谁是程序员鱼皮啊？"));
+        return queries;
+    }
+
+}
